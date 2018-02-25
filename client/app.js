8e9c2fa708d0db9634f7327cba3b10b6871de338
Jeremy Philipson <jeremyphilipson@gmail.com>
2018-02-25 16:07:35 -0500

    commit

--- client/app.js
+++ client/app.js
@@ -6,87 +6,58 @@ const emailInput = document.getElementById('email')
 const customerList = document.getElementById('customerList')
 const message = document.getElementById('message')
 const newMessage = document.createElement('h4')
+message.appendChild(newMessage)
 
-const emails = []
+const createCustomer = (customer) => {
+  const newCustomer = document.createElement('li')
+  newCustomer.style = 'margin:10px 0px';
+  newCustomer.className = 'list-group-item'
+  newCustomer.innerText = customer.email
+  // creating remove button
+  const removeButton = document.createElement('button')
+  removeButton.innerText = 'Remove'
+  removeButton.className = 'btn btn-outline-danger'
+  removeButton.style = 'margin-left:10px;'
+  customerList.appendChild(newCustomer)
+  newCustomer.appendChild(removeButton)
+  emailInput.value = ''
+  // remove button click event
+  removeButton.addEventListener('click', () => {
+    deleteCustomer(customer)
+    newCustomer.remove()
+  })
+}
 
+const deleteCustomer = (customer) => {
+  fetch(`/api/customers/${customer.id}`, {
+    headers: { 'Content-Type': 'application/json' },
+    method: 'delete',
+    body: JSON.stringify({ id: `${customer.id}` })
+  })
+}
+
+const checkForError = (response) => {
+  if (!response.ok) console.log('check4error', response)
+  return response
+}
+
+// fetching all customers
 fetch('/api/customers')
+  .then(response => checkForError(response))
   .then(result => result.json())
-  .then(data => {
-    data.forEach(person => {
-      // creating person list item
-      const newPerson = document.createElement('li')
-      newPerson.style = 'margin:10px 0px';
-      newPerson.className = 'list-group-item'
-      newPerson.innerText = person.email
-      newPerson.id = person.id
-      // creating remove button
-      const removeButton = document.createElement('button')
-      removeButton.innerText = 'Remove'
-      removeButton.className = 'btn btn-outline-danger'
-      removeButton.style = 'margin-left:10px;'
-      customerList.appendChild(newPerson)
-      newPerson.appendChild(removeButton)
-      // keeping track of emails
-      emails.push(person.email)
+  .then(data => data.forEach(customer => createCustomer(customer)))
+  .then(() => newMessage.innerText = 'Create a person!')
+  .catch(error => console.log('fetch', error))
 
-      removeButton.addEventListener('click', () => {
-        fetch(`/api/customers/${newPerson.id}`, {
-          headers: { 'Content-Type': 'application/json' },
-          method: 'delete',
-          body: JSON.stringify({ id: `${newPerson.id}` })
-        })
-        personIndex = emails.indexOf(newPerson.innerHTML.split('<')[0])
-        emails.splice(personIndex, 1)
-        newPerson.remove()
-      })
-
-    })
-  })
-  .then(() => {
-    newMessage.innerText = 'Create a person!'
-    message.appendChild(newMessage)
-  })
-  .catch(console.error)
-
-// adding new person to list
+// adding new customer to list
 formButton.addEventListener('click', () => {
   fetch('/api/customers', {
     headers: {'Content-Type': 'application/json'},
     method: 'post',
     body: JSON.stringify({email: emailInput.value})
   })
+  .then(response => checkForError(response))
   .then(result => result.json())
-  .then(customer => {
-    // creating new person list item
-    const newPerson = document.createElement('li')
-    newPerson.style = 'margin:10px 0px';
-    newPerson.className = 'list-group-item'
-    newPerson.innerText = customer.email
-    newPerson.id = customer.id
-    // creating remove button
-    const removeButton = document.createElement('button')
-    removeButton.innerText = 'Remove'
-    removeButton.className = 'btn btn-outline-danger'
-    removeButton.style = 'margin-left:10px;'
-    customerList.appendChild(newPerson)
-    newPerson.appendChild(removeButton)
-    // keeping track of emails
-    emails.push(customer.email)
-    // setting input field to empty after successfull submission
-    emailInput.value = ''
-
-    removeButton.addEventListener('click', () => {
-      fetch(`/api/customers/${newPerson.id}`, {
-        headers: { 'Content-Type': 'application/json' },
-        method: 'delete',
-        body: JSON.stringify({ id: `${newPerson.id}` })
-      })
-      personIndex = emails.indexOf(newPerson.innerHTML.split('<')[0])
-      emails.splice(personIndex, 1)
-      newPerson.remove()
-
-    })
-
-  })
-  .catch(console.error)
+  .then(customer => createCustomer(customer))
+  .catch(error => console.log('posting', error))
 })
